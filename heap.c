522dddeac0e0f5eeae2beaea5a34a811c4c0f01f
Gabriel Dolzan <gabrieldolzan@icloud.com>
2023-11-17 15:33:22 -0300

    Initial

---
+++ heap.c
@@ -0,0 +1,57 @@
+#include "heap.h"
+
+void swap(pair_t *a, pair_t *b) {
+    pair_t temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void maxHeapify(pair_t heap[], int size, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && heap[left].key > heap[largest].key) {
+        largest = left;
+    }
+
+    if (right < size && heap[right].key > heap[largest].key) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(&heap[i], &heap[largest]);
+        maxHeapify(heap, size, largest);
+    }
+}
+
+void heapifyUp(pair_t heap[], int *size, int pos) {
+    int parent = (pos - 1) / 2;
+
+    while (pos > 0 && heap[parent].key < heap[pos].key) {
+        swap(&heap[parent], &heap[pos]);
+        pos = parent;
+        parent = (pos - 1) / 2;
+    }
+}
+
+void insert(pair_t heap[], int *size, pair_t element) {
+    if (*size == 0) {
+        heap[0] = element;
+        (*size)++;
+    } else {
+        heap[*size] = element;
+        (*size)++;
+        heapifyUp(heap, size, *size - 1);
+    }
+}
+
+void decreaseMax(pair_t heap[], int size, pair_t element) {
+    if (size == 0) return;
+
+    if (heap[0].key > element.key) {
+        heap[0].key = element.key;
+        heap[0].val = element.val;
+        maxHeapify(heap, size, 0);
+    }
+}
---
+++ knn_mpi.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <mpi.h>
+#include <time.h>
+#include <float.h>
+#include <string.h>
+#include "heap.h"
+#include "chrono.c"
+
+// Estrutura para um ponto em D dimensões
+typedef struct {
+    float *coordinates;  // Coordenadas do ponto
+} Point;
+
+// Function to generate a set of random points in a flattened array
+void geraConjuntoDeDados(float *C, int nc, int D) {
+    for (int i = 0; i < nc * D; i++) {
+        C[i] = (float)rand() / RAND_MAX;
+    }
+}
+
+// Function to calculate the squared distance between two points in flattened arrays
+float squaredDistance(const float *p1, const float *p2, int D) {
+    float distance = 0.0;
+    for (int i = 0; i < D; i++) {
+        float diff = p1[i] - p2[i];
+        distance += diff * diff;
+    }
+    return distance;
+}
+
+// Function to perform the k-NN algorithm
+void knn(float *Q, int nq, float *P, int npp, int D, int k, int **resultIndices) {
+    for (int i = 0; i < nq; i++) {
+        pair_t *neighborsHeap = (pair_t *)malloc(k * sizeof(pair_t));
+        int heapSize = 0;
+
+        for (int j = 0; j < npp; j++) {
+            float distance = squaredDistance(&Q[i * D], &P[j * D], D);
+            pair_t neighbor = {distance, j};
+
+            if (heapSize < k) {
+                insert(neighborsHeap, &heapSize, neighbor);
+            } else if (distance < neighborsHeap[0].key) {
+                decreaseMax(neighborsHeap, k, neighbor);
+            }
+        }
+
+        for (int j = 0; j < k; j++) {
+            resultIndices[i][j] = neighborsHeap[j].val;
+        }
+
+        free(neighborsHeap);
+    }
+}
+
+// Function to verify the k-NN results
+void verificaKNN(float *Q, int nq, float *P, int n, int D, int k, int *R) {
+    int correto = 1;
+
+    for (int i = 0; i < nq; i++) {
+        float *distances = (float *)malloc(k * sizeof(float));
+        for (int j = 0; j < k; j++) {
+            distances[j] = FLT_MAX;
+        }
+
+        for (int j = 0; j < n; j++) {
+            float dist = squaredDistance(&Q[i * D], &P[j * D], D);
+
+            for (int l = k - 1; l >= 0; l--) {
+                if (dist < distances[l]) {
+                    if (l < k - 1) {
+                        distances[l + 1] = distances[l];
+                    }
+                    distances[l] = dist;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        for (int j = 0; j < k; j++) {
+            float resultDist = squaredDistance(&Q[i * D], &P[R[i * k + j] * D], D);
+            int found = 0;
+            for (int l = 0; l < k; l++) {
+                if (resultDist == distances[l]) {
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
+                correto = 0;
+                break;
+            }
+        }
+
+        free(distances);
+
+        if (!correto) {
+            printf("ERRADO\n");
+            return;
+        }
+    }
+    printf("CORRETO\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 5)
+    {
+        printf("Usage: %s <nq> <npp> <d> <k>\n", argv[0]);
+        return 1;
+    }
+
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+    chronometer_t chronometer;
+
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int nq = atoi(argv[1]);  // Number of points in Q
+    int npp = atoi(argv[2]); // Number of points in P
+    int d = atoi(argv[3]);   // Number of dimensions
+    int k = atoi(argv[4]);   // Number of nearest neighbors
+
+    float *P = (float *)malloc(npp * d * sizeof(float));
+    geraConjuntoDeDados(P, npp, d);
+
+    float *Q_flattened = NULL;
+    int *resultIndices = NULL;
+    if (world_rank == 0)
+    {
+        Q_flattened = (float *)malloc(nq * d * sizeof(float));
+        geraConjuntoDeDados(Q_flattened, nq, d);
+
+        resultIndices = (int *)malloc(nq * k * sizeof(int));
+    }
+
+    int local_nq = nq / world_size;
+    float *local_Q_flattened = (float *)malloc(local_nq * d * sizeof(float));
+    MPI_Scatter(Q_flattened, local_nq * d, MPI_FLOAT, local_Q_flattened, local_nq * d, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    int *localResultIndices = (int *)malloc(local_nq * k * sizeof(int));
+    int **resultIndices2D = (int **)malloc(local_nq * sizeof(int *));
+    for (int i = 0; i < local_nq; i++)
+    {
+        resultIndices2D[i] = &localResultIndices[i * k];
+    }
+
+    if(world_rank == 0)
+    {
+        chrono_reset(&chronometer);
+        chrono_start(&chronometer);
+    }
+
+    knn(local_Q_flattened, local_nq, P, npp, d, k, resultIndices2D);
+
+    int *gatheredResultIndices = NULL;
+    if (world_rank == 0)
+    {
+        gatheredResultIndices = (int *)malloc(nq * k * sizeof(int));
+    }
+
+    MPI_Gather(localResultIndices, local_nq * k, MPI_INT, gatheredResultIndices, local_nq * k, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Barrier to guarantee knn processing is already over
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (world_rank == 0)
+    {
+        chrono_stop(&chronometer);
+
+        double total_time_in_seconds = (double) chrono_gettotal(&chronometer) / 1000000000.0;
+        double MOPs = (double)nq / total_time_in_seconds;
+        printf("Tempo: %lf segundos\n", total_time_in_seconds);
+        printf("Throughput: %lf\n", MOPs);
+
+        verificaKNN(Q_flattened, nq, P, npp, d, k, gatheredResultIndices);
+        free(gatheredResultIndices);
+        free(Q_flattened);
+        free(resultIndices);
+    }
+
+    free(local_Q_flattened);
+    free(localResultIndices);
+    free(resultIndices2D);
+    free(P);
+
+    MPI_Finalize();
+    return 0;
+}
